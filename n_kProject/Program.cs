dbfa349b52b912cf20a2b88373538221dd511c25
MahdiSaadatiOrg <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-07-22 16:07:00 +0430

    Add files via upload

---
+++ n_kProject/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(N_kClass.N_K(4, 5));
+
+public static class N_kClass
+{
+    public static int N_K(int n , int k) 
+    {
+        if (n == k || k == 0) return 1;
+        
+        return (N_K(n-1, k-1)+N_K(n-1, k));
+    }
+}
